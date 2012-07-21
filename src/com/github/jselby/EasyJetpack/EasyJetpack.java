b71bc60fa2d1f4ca6779dd64c8684503da6d863f
j-selby <j-selby@github>
2012-07-21 18:23:24 +1000

    Change version number to v0.2

--- src/com/github/jselby/EasyJetpack/EasyJetpack.java
+++ src/com/github/jselby/EasyJetpack/EasyJetpack.java
@@ -2,7 +2,7 @@
  * EasyJetpack Minecraft Bukkit plugin
  * Written by j_selby
  * 
- * Version 0.1
+ * Version 0.2
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
